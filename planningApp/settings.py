685bb557e0f33b7991ee140360ab570c1ff72f53
GARajab <mohamed.rajab@ewa.bh>
2025-03-12 19:33:45 +0300

    Add requirements.txt

--- planningApp/settings.py
+++ planningApp/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-lp!wsq*w%riljr=6dcpzc#ix-7(_&y226*z86xzxaf5dtx@xl*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
