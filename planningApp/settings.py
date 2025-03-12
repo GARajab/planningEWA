8722e62902595547d294501021b79a2cede8c83f
GARajab <mohamed.rajab@ewa.bh>
2025-03-12 20:22:25 +0300

    try fix issues with render

--- planningApp/settings.py
+++ planningApp/settings.py
@@ -119,10 +119,16 @@ USE_TZ = True
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(
+    BASE_DIR, "staticfiles"
+)  # Directory for collected static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Additional directories for static files
+]
 
-# Required for production
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# Media files (uploads)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
